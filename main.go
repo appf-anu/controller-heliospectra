a6b9101fad9ece17519e0b9864f62b6295fe0bf7
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2019-04-12 11:34:44 +1000

    moved some stuff to the chamber_tools package

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bufio"
+	"github.com/appf-anu/chamber-tools"
 	"flag"
 	"fmt"
-	"github.com/bcampbell/fuzzytime"
 	"github.com/mdaffin/go-telegraf"
 	"github.com/ziutek/telnet"
 	"log"
@@ -18,17 +17,14 @@ import (
 
 var (
 	errLog     *log.Logger
-	ctx        fuzzytime.Context
-	zoneName   string
-	zoneOffset int
 )
 
 var (
-	noMetrics, dummy                           bool
-	address                                    string
-	multiplier                                 float64
+	noMetrics, dummy, loopFirstDay            bool
+	address                                   string
+	multiplier                                float64
 	conditionsPath, hostTag, groupTag, didTag string
-	interval                                   time.Duration
+	interval                                  time.Duration
 )
 
 const (
@@ -80,20 +76,6 @@ quirks:
 	fmt.Printf(use, os.Args[0], os.Args[0], os.Args[0])
 }
 
-func parseDateTime(tString string) (time.Time, error) {
-
-	datetimeValue, _, err := ctx.Extract(tString)
-	if err != nil {
-		errLog.Printf("couldn't extract datetime: %s", err)
-	}
-
-	datetimeValue.Time.SetHour(datetimeValue.Time.Hour())
-	datetimeValue.Time.SetMinute(datetimeValue.Time.Minute())
-	datetimeValue.Time.SetSecond(datetimeValue.Time.Second())
-	datetimeValue.Time.SetTZOffset(zoneOffset)
-
-	return time.Parse("2006-01-02T15:04:05Z07:00", datetimeValue.ISOFormat())
-}
 
 func execCommand(conn *telnet.Conn, command string) (ret string, err error) {
 	// write command
@@ -146,82 +128,7 @@ func chompAllStrings(conn *telnet.Conn, command string) (values []string, err er
 	return
 }
 
-func runConditions() {
-	errLog.Printf("running conditions file: %s\n", conditionsPath)
-	file, err := os.Open(conditionsPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	idx := 0
-	var lastTime time.Time
-	var lastLineSplit []string
-	firstRun := true
-	for scanner.Scan() {
-		line := scanner.Text()
-		if idx == 0 {
-			idx++
-			continue
-		}
 
-		lineSplit := strings.Split(line, ",")
-		timeStr := lineSplit[0]
-		theTime, err := parseDateTime(timeStr)
-		if err != nil {
-			errLog.Println(err)
-			continue
-		}
-
-		// if we are before the time skip until we are after it
-		// the -10s means that we shouldnt run again.
-		if theTime.Before(time.Now()) {
-			lastLineSplit = lineSplit
-			lastTime = theTime
-			continue
-		}
-
-		if firstRun {
-			firstRun = false
-			errLog.Println("running firstrun line")
-			for i := 0; i < 10; i++ {
-				if runStuff(lastTime, lastLineSplit) {
-					break
-				}
-			}
-		}
-
-		errLog.Printf("sleeping for %ds\n", int(time.Until(theTime).Seconds()))
-		time.Sleep(time.Until(theTime))
-
-		// RUN STUFF HERE
-		for i := 0; i < 10; i++ {
-			if runStuff(theTime, lineSplit) {
-				break
-			}
-		}
-		// end RUN STUFF
-		idx++
-	}
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
-func minMax(value int) int {
-	return min(max(value, 0), 1000)
-}
 
 func intToString(a []int) []string {
 	b := make([]string, len(a))
@@ -240,11 +147,8 @@ func setMany(conn *telnet.Conn, values []int) (err error) {
 	return
 }
 
-func getPower(conn *telnet.Conn) (values []float64, err error) {
-	intValues, err := chompAllInts(conn, "getAllRelPower")
-	for _, v := range intValues{
-		values = append(values, float64(v))
-	}
+func getPower(conn *telnet.Conn) (values []int, err error) {
+	values, err = chompAllInts(conn, "getAllRelPower")
 	return
 }
 
@@ -284,33 +188,31 @@ func runStuff(theTime time.Time, lineSplit []string) bool {
 		return false
 	}
 
-
 	wavelengths, err := getWl(conn)
 	if err != nil {
 		errLog.Println(err)
 		return false
 	}
-	minLength := min(len(wavelengths),len(lightValues))
-	if len(lightValues) != minLength{
+	minLength := chamber_tools.Min(len(wavelengths), len(lightValues))
+	if len(lightValues) != minLength {
 		errLog.Println("Different number of light values than wavelengths")
 	}
 	intVals := make([]int, minLength)
-	for i,x := range lightValues{
-		intVals[i] = minMax(int(x*multiplier))
+	for i, x := range lightValues {
+		intVals[i] = chamber_tools.Clamp(int(x * multiplier), 0, 1000)
 	}
 	err = setMany(conn, intVals)
 
-	if err != nil{
+	if err != nil {
 		errLog.Println(err)
 		return false
 	}
 	errLog.Println("scaling ", multiplier)
 	errLog.Println("ran ", theTime.Format("2006-01-02T15:04:05"), intVals)
 
-
-	time.Sleep(time.Millisecond*50)
+	time.Sleep(time.Millisecond * 50)
 	returnedLv, err := getPower(conn)
-	if err != nil{
+	if err != nil {
 		errLog.Println(err)
 		return false
 	}
@@ -327,7 +229,7 @@ func runStuff(theTime time.Time, lineSplit []string) bool {
 	return true
 }
 
-func writeMetrics(wavelengths []string, lightValues []float64) error {
+func writeMetrics(wavelengths []string, lightValues []int) error {
 	if !noMetrics {
 		telegrafHost := "telegraf:8092"
 		if os.Getenv("TELEGRAF_HOST") != "" {
@@ -340,22 +242,22 @@ func writeMetrics(wavelengths []string, lightValues []float64) error {
 		}
 		defer telegrafClient.Close()
 
-		m := telegraf.NewMeasurement("helio-light")
+		m := telegraf.NewMeasurement("heliospectra-light")
 		if len(wavelengths) != len(lightValues) {
 			return fmt.Errorf("wavelengths and light values differ")
 		}
 
 		for i, v := range lightValues {
-			wl,err := strconv.ParseInt(wavelengths[i], 10, 64)
-			if err != nil{
+			wl, err := strconv.ParseInt(wavelengths[i], 10, 64)
+			if err != nil {
 				errLog.Println(err)
 				continue
 			}
 			if wl == 6500 {
-				m.AddFloat64(fmt.Sprintf("%dk", wl), v)
+				m.AddInt(fmt.Sprintf("%dk", wl), v)
 				continue
 			}
-			m.AddFloat64(fmt.Sprintf("%dnm", wl), v)
+			m.AddInt(fmt.Sprintf("%dnm", wl), v)
 		}
 		if hostTag != "" {
 			m.AddTag("host", hostTag)
@@ -385,12 +287,7 @@ func init() {
 
 	errLog = log.New(os.Stderr, "[heliospectra] ", log.Ldate|log.Ltime|log.Lshortfile)
 	// get the local zone and offset
-	zoneName, zoneOffset = time.Now().Zone()
 
-	ctx = fuzzytime.Context{
-		DateResolver: fuzzytime.DMYResolver,
-		TZResolver:   fuzzytime.DefaultTZResolver(zoneName),
-	}
 	flag.Usage = usage
 	flag.BoolVar(&noMetrics, "no-metrics", false, "dont collect metrics")
 	if tempV := strings.ToLower(os.Getenv("NO_METRICS")); tempV != "" {
@@ -407,6 +304,15 @@ func init() {
 			dummy = true
 		} else {
 			dummy = false
+		}
+	}
+
+	flag.BoolVar(&loopFirstDay, "loop", false, "loop over the first day")
+	if tempV := strings.ToLower(os.Getenv("LOOP")); tempV != "" {
+		if tempV == "true" || tempV == "1" {
+			loopFirstDay = true
+		} else {
+			loopFirstDay = false
 		}
 	}
 
@@ -452,7 +358,6 @@ func init() {
 		os.Exit(1)
 	}
 
-	errLog.Printf("timezone: \t%s\n", zoneName)
 	errLog.Printf("hostTag: \t%s\n", hostTag)
 	errLog.Printf("groupTag: \t%s\n", groupTag)
 	errLog.Printf("address: \t%s\n", address)
@@ -476,12 +381,12 @@ func main() {
 			}
 
 			lightPower, err := getPower(conn)
-			if err != nil{
+			if err != nil {
 				errLog.Println(err)
 				return
 			}
 			lightWavelengths, err := getWl(conn)
-			if err != nil{
+			if err != nil {
 				errLog.Println(err)
 				return
 			}
@@ -503,7 +408,7 @@ func main() {
 	}
 
 	if conditionsPath != "" && !dummy {
-		runConditions()
+		chamber_tools.RunConditions(errLog, runStuff, conditionsPath, loopFirstDay)
 	}
 
 }
